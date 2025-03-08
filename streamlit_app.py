727c08fe93822365b79b1b3e42809dec5f08ddc5
Elizabeth Rosso <32004735+lizprogrammer@users.noreply.github.com>
2025-03-08 22:28:17 +0000

    SEARCH on

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,11 @@ st.write("Choose the fruits you want in your custom Smoothie!"
 )
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, use_container_width=True)
+
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
